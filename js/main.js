d0d6f70f0753513341bb0fd3a43e233dbb3cac54
Caioscg <caio.gonzaga@discente.ufg.br>
2022-12-20 20:39:32 -0300

    js arqs

--- js/main.js
+++ js/main.js
@@ -1,30 +1,24 @@
+import { Modal } from './modal.js'
+
 const weight = document.querySelector("#weight")
 const height = document.querySelector("#height")
 const form = document.querySelector("form")
 const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
-const h1 = document.querySelector(".screen2 h1")
-const pop = document.querySelector(".close")
 
 let imc
-
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     imcCalc()
 })
 
-pop.addEventListener("click", restart)
-
 function imcCalc() {
     imc = weight.value/((height.value/100) ** 2)
-    
-    screen2.classList.add("open")
-    h1.innerText = `Seu IMC é de ${imc.toFixed(2)}`
+    Modal.open()
+    Modal.h1.innerText = `Seu IMC é de ${imc.toFixed(2)}`
 }
 
 function restart() {
     weight.value = ""
     height.value = ""
-    screen2.classList.remove("open")
 }
---
+++ js/modal.js
@@ -0,0 +1,22 @@
+const weight = document.querySelector("#weight")
+const height = document.querySelector("#height")
+
+export const Modal = {
+
+    screen2: document.querySelector(".screen2"),
+    h1: document.querySelector(".screen2 h1"),
+    pop: document.querySelector(".close"),
+
+    open() {
+        Modal.screen2.classList.add("open")
+    },
+
+    close() {
+        Modal.screen2.classList.remove("open")
+        weight.value = ""
+        height.value = ""
+    }
+
+}
+
+Modal.pop.addEventListener("click", Modal.close)
